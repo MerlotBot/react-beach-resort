03acd58039e26fb735999bbd419c0a78b733e990
Nikolaos Antonakakis <n.antonakakis@gmail.com>
2019-08-05 14:54:32 +0300

    Room, Services, Title, FeaturedRooms, Loading components, 2:21:35

---
+++ src/components/Services.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
+import Title from "./Title";
+
+export default class Services extends Component {
+  state = {
+    services: [
+      {
+        icon: <FaCocktail />,
+        title: "Free cocktails",
+        info:
+          "Mollit ipsum officia sint cupidatat cupidatat do non irure dolor adipisicing."
+      },
+      {
+        icon: <FaHiking />,
+        title: "Endless Hiking",
+        info:
+          "Mollit ipsum officia sint cupidatat cupidatat do non irure dolor adipisicing."
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: "Free Shuttle",
+        info:
+          "Mollit ipsum officia sint cupidatat cupidatat do non irure dolor adipisicing."
+      },
+      {
+        icon: <FaBeer />,
+        title: "Strongest Beer",
+        info:
+          "Mollit ipsum officia sint cupidatat cupidatat do non irure dolor adipisicing."
+      }
+    ]
+  };
+  render() {
+    return (
+      <section className="services">
+        <Title title="services" />
+        <div className="services-center">
+          {this.state.services.map((item, index) => {
+            return (
+              <article key={index} className="service">
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
+}
