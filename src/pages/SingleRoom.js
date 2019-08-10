5458b54eab9f3fff8f0d9128af487676a8ac50ad
Nikolaos Antonakakis <n.antonakakis@gmail.com>
2019-08-10 08:21:44 +0200

    Finished react beach application

--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import defaultBcg from "../images/room-1.jpeg";
-import Hero from "../components/Hero";
+// import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../context";
@@ -21,7 +21,7 @@ export default class SingleRoom extends Component {
   render() {
     const { getRoom } = this.context;
     const room = getRoom(this.state.slug);
-    console.log(room);
+    // console.log(room);
     if (!room) {
       return (
         <div className="error">
